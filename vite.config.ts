24d9479264738e9efcf06e6a67e92c5a7639ae33
yyf999999999 <122949113+yyf999999999@users.noreply.github.com>
2024-11-14 09:10:38 +0900

    package vite.config 変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path"; // ◀◀ 追加
+
+const repositoryName = "react-todo-app"; // ◀◀ 追加
 
 export default defineConfig({
   plugins: [react()],
+  // ▼▼ 追加 ここから ▼▼
+  base: process.env.NODE_ENV === "production" ? `/${repositoryName}/` : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        404: path.resolve(__dirname, "404.html"),
+      },
+    },
+  },
+  // ▲▲ 追加 ここまで ▲▲
   server: {
-    port: 3000, // デフォルトのポートを3000に設定
+    port: 3000,
     strictPort: false,
     open: true,
   },
